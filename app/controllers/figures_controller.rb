58635bddce0b448c821fbd9d108dcad1439ed22a
Kelsey Fecho <kfecho30@gmail.com>
2018-04-20 19:48:17 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,21 +16,12 @@ class FiguresController < ApplicationController
     @figure.landmarks << Landmark.create(:name => params[:landmark][:name]) if !params[:landmark][:name].empty?
     @figure.titles << Title.create(:name => params[:title][:name]) if !params[:title][:name].empty?
 
-    if !params[:title][:name].empty?
-      @figure.titles << Title.create(params[:title])
-    end
     #params[:figure][:landmark_ids].each do |landmark|
     #  @figure.landmarks << Landmark.find(landmark)
-    #end
-    if !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(params[:landmark])
-    end
-    #params[:figure][:title_ids].each do |title|
-    #  @figure.titles << Title.find_by(:name =>title)
     #end
 
-    #params[:figure][:landmark_ids].each do |landmark|
-    #  @figure.landmarks << Landmark.find(landmark)
+    #params[:figure][:title_ids].each do |title|
+    #  @figure.titles << Title.find_by(:name =>title)
     #end
 
     @figure.save
