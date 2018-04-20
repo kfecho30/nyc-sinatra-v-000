21d381e5bbd1aa51d9a2857a30d470c84bbdd39d
Kelsey Fecho <kfecho30@gmail.com>
2018-04-20 21:02:31 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -7,4 +7,8 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :"/landmarks/new"
   end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+  end
 end
