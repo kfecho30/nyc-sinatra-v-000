464c50ad280fe15afdd09766b4c7d3a04c2bac42
Kelsey Fecho <kfecho30@gmail.com>
2018-04-20 20:36:19 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,7 +12,7 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     binding.pry
-    @figure = Figure.create(:name => params[:figure][:name])
+    @figure = Figure.create(:name => params[:figure])
 
     @figure.landmarks << Landmark.create(:name => params[:landmark][:name]) if !params[:landmark][:name].empty?
     @figure.titles << Title.create(:name => params[:title][:name]) if !params[:title][:name].empty?
