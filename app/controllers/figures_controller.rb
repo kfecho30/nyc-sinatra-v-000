3ead2fab059c7862c151f3d91052ac9b6b7b58f7
Kelsey Fecho <kfecho30@gmail.com>
2018-04-20 19:42:30 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -17,7 +17,7 @@ class FiguresController < ApplicationController
     @figure.titles << Title.create(:name => params[:title][:name]) if !params[:title][:name].empty?
 
     params[:figure][:title_ids].each do |title|
-      @figure.titles << Title.find(title)
+      @figure.titles << Title.find_by(:name =>title)
     end
 
     params[:figure][:landmark_ids].each do |landmark|
